756af746a89f4f45e84722bb6330f2051f8b4471
Brandon Howell <bshowell@calpoly.edu>
2023-12-01 18:52:57 -0800

    Initial commit

---
+++ Nicknames/Nicknames.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace Nicknames
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Nicknames : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ Nicknames/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Nicknames
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "bshowell-Nicknames";
+        public const string PLUGIN_NAME = "Nicknames";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
